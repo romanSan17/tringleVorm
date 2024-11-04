3d1f3e10c875061b512ba696406f60fc673d6e8b
opilane <opilane@HP-CZC2349HTG>
2024-11-04 15:46:10 +0200

    Add project files.

---
+++ tringleVorm/Triangle.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace tringleVorm
+{
+    internal class Triangle
+    {
+        public double a;
+        public double b;
+        public double c;
+
+        public Triangle(double A, double B, double C)
+        {
+            a = A;
+            b = B;
+            c = C;
+        }
+
+        public string OutputA()
+        {
+            return Convert.ToString(a);
+        }
+        public string OutputB()
+        {
+            return Convert.ToString(b);
+        }
+        public string OutputC()
+        {
+            return Convert.ToString(c);
+        }
+
+        public double Perimeter()
+        {
+            return a + b + c;
+        }
+        public double Surface()
+        {
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+
+        public double GetSetA
+        {
+            get { return a; }
+            set { a = value; }
+        }
+        public double GetSetB
+        {
+            get { return b; }
+            set { b = value; }
+        }
+        public double GetSetC
+        {
+            get { return c; }
+            set { c = value; }
+        }
+
+        public bool ExistTriangle
+        {
+            get
+            {
+                return (a + b > c) && (a + c > b) && (b + c > a);
+            }
+        }
+    }
+}
